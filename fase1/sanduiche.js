8c0ede5db73e3e9f2369b0375779d75267fa1309
zsazsaspeck <giovannemenezeslopes@hotmail.com>
2021-03-26 14:07:45 -0300

    sanduiche

---
+++ fase1/sanduiche.js
@@ -0,0 +1,17 @@
+
+var mortadela = "300 gramas";
+var pão = "fatia de pão de forma"
+var azeite = "1 colher de sopa"
+console.log("Ingredientes:");
+console.log(mortadela, "de mortadela");
+console.log("2", pão);
+console.log(azeite, "de azeite\n");
+console.log("Modo de Preparo:")
+console.log("Espalhe na frigideira as", mortadela, "de mortadela;");
+console.log("Coloque na frigideira", azeite, "de azeite;");
+console.log("Deixe fritar por 1 minuto;");
+console.log("coloque uma", pão, "em um prato");
+console.log("por cima da", pão, "coloque a mortadela frita;");
+console.log("Por cima da mortadela frita coloque a ultima", pão);
+
+
