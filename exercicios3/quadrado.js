61267fc98386b3d0205bfb6c44fa5ed65a27068b
zsazsaspeck <81425263+zsazsaspeck@users.noreply.github.com>
2021-03-30 15:48:49 -0300

    Delete quadrado.js

--- exercicios3/quadrado.js
+++
@@ -1,8 +0,0 @@
-//O programa “Quadrado” terá um código adaptado do item anterior, para calcular o quadrado do número digitado
-
-const prompt = require("prompt-sync")();
-
-var x = prompt("Digite a medida do lado do quadrado:");
-var y = x * x;
-var z = x * 4;
-console.log("A área do quadrado de lado", x, "cm é", y, "cm e seu perímetro é", z,"cm");
