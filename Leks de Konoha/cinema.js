8d8aefa77770087cfb6cf574a7fd3cb30edf0181
zsazsaspeck <81425263+zsazsaspeck@users.noreply.github.com>
2021-04-09 11:11:35 -0300

    Devs e Gamers

--- Leks de Konoha/cinema.js
+++ Leks de Konoha/cinema.js
@@ -1,4 +1,4 @@
-/*1. Um cinema faz reservas de 5 ingressos para uma sala, sendo 2 para meia 
+/ *1. Um cinema faz reservas de 5 ingressos para uma sala, sendo 2 para meia 
 entrada, para a reserva, basta informar o nome e se é meia ou não, é 
 permitido apenas uma reserva por nome.
 2. Após os 5 reservados, o cliente pode retirar se ingresso informando o seu 
