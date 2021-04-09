c079389ae06dd0f153204cf635dffef5aadac89b
zsazsaspeck <81425263+zsazsaspeck@users.noreply.github.com>
2021-04-09 11:15:56 -0300

    Salão da Leila

--- Leks de Konoha/leilaflix.js
+++ Leks de Konoha/leilaflix.js
@@ -1,4 +1,4 @@
-//Leilaflix
+// Leilaflix
 //Este é um canal de stream de filmes de Horror e Drama.
 //Apresente uma lista final de filmes e atrelar a relevância dela de acordo com as escolhas do usuário durante a interação com a lista de filmes exibidos no canal (exemplo da Netflix, Spotify, Youtube)
 //Dica: categorize a relevância dos filmes, afunilando a preferência do usuário a partir da seleção dos filmes que contém tópicos do interesse do usuário.
