2324df44ca050f8b16e2aeb3c9ec33a970809bf4
zsazsaspeck <81425263+zsazsaspeck@users.noreply.github.com>
2021-04-20 15:35:02 -0300

    Delete exercicios5 directory

--- exercicios5/universidade.js
+++
@@ -1,8 +0,0 @@
-//Os boletos da "Universidade" são emitidos com vencimento no dia 15 do mês. Para os pagamentos recebidos entre os dias 10 a 13, será concedido um desconto na próxima mensalidade. O programa irá receber o número do boleto e o dia do pagamento, e emitir o extrato.
-
-const ler = require("prompt-sync")();
-var today = Number(ler("Que dia é hoje?"))
-var boleto = ler("Qual é o nº do boleto? ")
-var desconto = (today < 14 && today >= 10)
-if (desconto)console.log("Desconto na próxima mensalidade", "para o boleto", boleto, ":", desconto);
-else console.log("Desconto na próxima mensalidade para o boleto?", boleto, ":", desconto);
